5c8610e50146389b48eccdf649e8ae04556413ff
Meet Parmar <121307496+immeeeet@users.noreply.github.com>
2026-07-11 19:56:46 +0530

    Add: binary-search - submission-0

---
+++ Data Structures & Algorithms/binary-search/submission-0.py
@@ -0,0 +1,7 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        if target not in nums:
+            return -1
+        for i,n in enumerate(nums):
+            if target==n:
+                return i
